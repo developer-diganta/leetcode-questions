74d1880e4d4b9070539eb6121592ed0047d5d7fa
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-01-04 23:24:40 +0530

    Time: 149 ms (5.55%), Space: 40.6 MB (49.37%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -2,14 +2,12 @@
  * @param {string[]} sentences
  * @return {number}
  */
-function max(a,b){
-    return a>b?a:b;
-}
+
 var mostWordsFound = function(sentences) {
     var len = 0;
     sentences.map((x)=>{
         const a = x.split(" ");
-        len = max(a.length,len);
+        len = a.length>len?a.length:len;
     })
     return len;
 };
