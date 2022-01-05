e6e4a4bd049a3381be1dcb1870ac8dbd2b547388
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-01-06 01:50:10 +0530

    Time: 104 ms (18.60%), Space: 39.8 MB (95.40%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
@@ -3,9 +3,6 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-   a = s.split(" ").filter((c)=>c!=='')
-   b=""
-   for (i=a.length-1;i>=0;i--)
-        b=b+" "+a[i];
-    return b.trim();
+    return s.split(" ").reverse().join(' ').replace(/\s+/g, " ").trim()
+
 };
