c6d0b355658fa2e1ab45dbed86218a15102df790
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-01-07 12:54:19 +0530

    Time: 116 ms (14.93%), Space: 40.5 MB (89.23%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
@@ -4,20 +4,16 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    var left = 0
-    var right = letters.length - 1
-    var boundaryIndex = -1
-    
-    while (left <= right) {
-        var mid = Math.floor((left + right) / 2)
-        if (letters[mid] > target) {
-            boundaryIndex = mid
-            right = mid - 1
-        } else {
-            left = mid + 1
+    var s=0,e=letters.length-1;
+    var m=0
+    while(s<=e){
+        m=Math.floor((s+e)/2)
+        if(letters[m]<=target){
+            s=m+1;
+        }
+        else{
+            e=m-1
         }
     }
-    
-    return letters[left % letters.length]
-
+    return letters[s%letters.length];
 };
