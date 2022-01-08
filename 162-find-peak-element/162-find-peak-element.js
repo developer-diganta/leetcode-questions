8a9eeadc0e3cd426c809da9b92f157ea2a496541
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-01-08 12:53:51 +0530

    Time: 73 ms (56.70%), Space: 38.5 MB (86.43%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(arr) {
+    var s=0,e=arr.length-1,mid=0;
+    while(s!==e){
+        mid=Math.floor((s+e)/2);
+        if(arr[mid]<arr[mid+1]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+    }
+    return s;
+};
