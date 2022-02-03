a9d6455e79422c56923eecb9cc726f2d3fb54cbe
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-02-03 19:04:44 +0530

    Time: 80 ms (95.96%), Space: 44 MB (5.75%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    var s = 0;
+    var e=x;
+    var ans=0;
+    while(s<=e){
+        var mid = Math.floor((s+e)/2);
+        if(mid*mid===x){
+            return mid;
+        }
+        else if(mid*mid<x){
+            s=mid+1;
+            ans=mid
+        }
+        else
+            e=mid-1;
+    }
+    return ans;
+};
