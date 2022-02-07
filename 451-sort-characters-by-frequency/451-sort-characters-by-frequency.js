6f3efd310d133d8324966bdcbfe2c61530c7de9c
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-02-07 21:25:15 +0530

    Time: 121 ms (52.07%), Space: 45 MB (65.58%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
@@ -4,7 +4,6 @@
  */
 var frequencySort = function(s) {
     var ob = {}
-    var arr =[];
     for(var i=0;i<s.length;i++){
         if(s[i] in ob){
             ob[s[i]]=ob[s[i]]+1;
@@ -13,18 +12,18 @@ var frequencySort = function(s) {
             ob[s[i]]=1;
         }
     }
-    var sortable = [];
-for (var vehicle in ob) {
-    sortable.push([vehicle, ob[vehicle]]);
-}
+    var arr = [];
+    for (var a in ob) {
+        arr.push([a, ob[a]]);
+    }
 
-sortable.sort(function(a, b) {
-    return b[1] - a[1];
-});
-    console.log(sortable)
+    arr.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    console.log(arr)
     var ans = "";
-    for(var i=0;i<sortable.length;i++){
-        ans+=sortable[i][0].repeat(sortable[i][1]);
+    for(var i=0;i<arr.length;i++){
+        ans+=arr[i][0].repeat(arr[i][1]);
     }
     console.log(ans)
     return ans;
