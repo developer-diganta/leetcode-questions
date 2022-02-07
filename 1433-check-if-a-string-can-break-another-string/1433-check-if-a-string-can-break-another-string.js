a764de5cc6f8cf93775fd3fc69cfdebe4bea4e02
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-02-07 22:05:27 +0530

    Time: 244 ms (88.64%), Space: 58.5 MB (15.91%) - LeetHub

---
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+function get_X_greater(s1, s2,l){
+        for(var i=l; i<s1.length; i++){
+            if(s2[i] > s1[i]) return false;
+        }
+        return true;
+    }
+function get_Y_greater(s1, s2, l){
+        for(var i=l; i<s1.length(); i++){
+            if(s1[i] > s2[i]) return false;
+        }
+        return true;
+    }
+    
+var checkIfCanBreak = function(s1, s2) {
+    s3=s1.split("").sort().reverse();
+    s4=s2.split("").sort().reverse();
+    var cmp;
+    var i=0;
+    var flag=true;
+    for(;i<s3.length;i++)
+    {
+        if(s3[i]!==s4[i]){
+        if(s3[i]>s4[i]){
+           flag = get_X_greater(s3,s4,i);
+        }
+        else{
+            flag = get_X_greater(s4,s3,i);
+        }
+            break;
+        }
+    }
+    return flag;
+};
