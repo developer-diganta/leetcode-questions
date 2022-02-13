0e2b1b3a50fa9da032b89eabb4f95c7ee16cc92d
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-02-13 18:05:51 +0530

    Time: 60 ms (98.18%), Space: 42.7 MB (30.16%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.js
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -3,22 +3,25 @@
  * @return {string[]}
  */
 
-
 var generateParenthesis = function(n) {
     var sol = [];
-    var solve = function(n,o,c,pat){
-    if(c===n && o===n){
+
+    var solve = function(o,c,pat){
+    // console.log(o," ",c);
+    if(o===c && o===0){
         sol.push(pat);
         return;
     }
-    if(o<n){
-        solve(n,o+1,c,pat+"(");
+    if(o!==0 && o<=c){
+
+        solve(o-1,c,pat+"(");
     }
-    if(c<o){
-        solve(n,o,c+1,pat+")");
+    if(c!==0 && c>=o){
+
+        solve(o,c-1,pat+")");
     }
     
 }
-    solve(n,0,0,"");
+    solve(n,n,"");
     return sol;
 };
