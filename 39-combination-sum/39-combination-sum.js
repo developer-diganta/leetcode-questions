705458d37d3763461e45a46afc7f231db588040f
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-02-14 12:52:32 +0530

    Time: 230 ms (10.96%), Space: 48 MB (16.80%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+
+
+var combinationSum = function(candidates, target) {
+
+var obj = {ans:[]}
+    var abc = (curr,i,t) => {
+        if(t===target){
+            console.log(curr)
+            obj.ans.push([...curr]);
+            return;
+        }
+        if(i>=candidates.length || t>target){
+            return ;
+        }
+        curr.push(candidates[i]);
+        abc(curr,i,t+candidates[i]);
+        curr.pop();
+        abc(curr,i+1,t);
+    }
+    // var curr = [];
+
+    abc([],0,0);
+    return obj.ans;
+    
+};
