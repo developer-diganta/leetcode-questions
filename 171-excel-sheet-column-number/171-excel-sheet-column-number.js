50439ff1895216a9b2e7ebe89bc67a754907c76a
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-02-22 22:23:50 +0530

    Time: 225 ms (5.00%), Space: 45.6 MB (5.56%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.js
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.js
@@ -6,7 +6,6 @@ var titleToNumber = function(columnTitle) {
         var  result = 0;
         for(var i=0;i<columnTitle.length;i++)
         {
-            var c = columnTitle[i];
             var d = columnTitle.charCodeAt(i) -64;
         console.log(d)
             result = result * 26 + d;
