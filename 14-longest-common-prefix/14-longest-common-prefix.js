5112f8f0a27b61f2895ff6740bba3b7ffc823552
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-02-25 23:55:50 +0530

    Time: 105 ms (41.94%), Space: 42.6 MB (28.46%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (strs.length === 0) return '';
+  let r = '';
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return r;
+    }
+    r += strs[0][i];
+  }
+  return r;
+};
