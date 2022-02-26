d9be2530acadfcecd0d2ea040ee21d48263da9aa
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-02-26 22:25:50 +0530

    Time: 109 ms (82.61%), Space: 45 MB (54.01%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+   for(i=0;i<s.length;i++){
+       if (s.indexOf(s[i])===s.lastIndexOf(s[i])){
+          return i;
+      } 
+   }
+   return -1;
+};
