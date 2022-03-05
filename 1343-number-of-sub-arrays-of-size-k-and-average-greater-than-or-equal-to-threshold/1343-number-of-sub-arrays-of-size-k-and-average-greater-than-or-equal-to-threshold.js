fb95f4d09d88b21517766157a3bb970c3e8cb42e
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-03-05 18:19:06 +0530

    Time: 2922 ms (7.60%), Space: 53.4 MB (5.85%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
@@ -4,13 +4,6 @@
  * @param {number} threshold
  * @return {number}
  */
-var getAverage = (n)=>{
-    var s=0;
-    for(var i=0;i<n.length;i++){
-        s=s+n[i];
-    }
-    return s/n.length;
-}
 var numOfSubarrays = function(arr, k, threshold) {
     var pre=[];
     for(var i=0;i<=arr.length-k;i++){
