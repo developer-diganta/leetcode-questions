b73ebdfcccead0316ea38181c75359a9b2ac79ff
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-03-14 22:36:43 +0530

    Time: 175 ms (51.30%), Space: 63.9 MB (44.72%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+      const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i - map[nums[i]] <= k) return true;
+    map[nums[i]] = i;
+  }
+
+  return false;
+};
