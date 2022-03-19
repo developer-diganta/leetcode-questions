fd373b8e9c0ccdb0ff94087709d0d56ec574c109
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-03-19 11:53:46 +0530

    Time: 97 ms (30.78%), Space: 42.3 MB (47.75%) - LeetHub

---
+++ 100-same-tree/100-same-tree.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+        if(p === null || q===null) return (p===q);
+        return (p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
+};
