0a9bb934c0bf9e90cb7ae83e5cc28c25a28eb3ed
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-03-27 22:43:22 +0530

    Time: 96 ms (52.23%), Space: 44.6 MB (41.40%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+var kWeakestRows = function(mat, k) {
+      return mat
+    .map((row, index) => [index, row.filter(n => n === 1).length])
+    .sort(([i1, c1], [i2, c2]) => c1 - c2 || i1 - i2)
+    .map(([index]) => index)
+    .slice(0, k);
+};
