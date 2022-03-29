9e1f089cab094dc65f7732989e50be20f3688a04
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-03-30 00:11:35 +0530

    Time: 288 ms (11.53%), Space: 49.1 MB (19.71%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const m = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900
+}
+
+    return s.split(/(IV|IX|XL|XC|CD|CM|I|V|X|L|C|D|M)/)
+	.filter(Boolean)
+	.map(x => m[x])
+	.reduce((a, b) => a + b, 0)
+};
