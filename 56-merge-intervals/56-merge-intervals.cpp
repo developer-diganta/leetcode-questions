7746ea8c23a4e1d7c617c97270a666bae31432ce
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-05-04 13:21:02 +0530

    Time: 55 ms (39.80%), Space: 20 MB (27.86%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool cmp(vector<int>a,vector<int>b){
+        return a[0]<b[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+                vector<vector<int>> merged;
+
+        for(auto interval:intervals){
+            if (merged.empty() || merged.back()[1] < interval[0]) {
+                merged.push_back(interval);
+            }
+            else{
+                merged.back()[1] = max(merged.back()[1], interval[1]);
+
+            }
+        }
+        return merged;
+    }
+};
