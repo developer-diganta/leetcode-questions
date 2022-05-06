17b5e2dede8d706ad27ebd59228a844677f5524e
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-05-06 11:59:40 +0530

    Time: 539 ms (54.18%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,9 @@
+
+SELECT employee_id 
+FROM employees
+WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
+UNION
+SELECT employee_id 
+FROM salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY employee_id;
