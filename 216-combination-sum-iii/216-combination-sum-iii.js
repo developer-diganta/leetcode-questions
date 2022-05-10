59309396680192c0afd2f91cbfff9e64d73fcd81
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-05-10 22:47:55 +0530

    Time: 82 ms (41.61%), Space: 44.2 MB (8.52%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.js
+++ 216-combination-sum-iii/216-combination-sum-iii.js
@@ -1,16 +1,11 @@
 var combinationSum3 = function(k, n) {
     let res = [];
-    
     function helper(nums, sum){
-        // we have filled out all K options
         if(nums.length === k){
-            // do the filled options sum up to n?
             if(sum === n)
-                // we found a solution
                 res.push(nums)
             return;
         }
-        // look at the next number by checking last evaluated value
         let  i = nums.length > 0 ? nums[nums.length - 1] + 1 : 1;
         for(; i < 10; i++)
             helper([...nums, i], sum + i)
