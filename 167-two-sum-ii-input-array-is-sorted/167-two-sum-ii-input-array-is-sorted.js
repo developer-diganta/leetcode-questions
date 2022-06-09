e761f9a0bafb8d5792e0824553228d357f76bf01
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-06-09 23:45:53 +0530

    Time: 83 ms (56.65%), Space: 43.3 MB (19.20%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,14 @@
+const twoSum = (numbers, target) => {
+    let p1 = 0
+    let p2 = numbers.length - 1
+    
+    while (numbers[p1] + numbers[p2] !== target) {
+        if (numbers[p1] + numbers[p2] > target) {
+            p2--
+        } else {
+            p1++
+        }
+    }
+    
+    return [p1 + 1, p2 + 1]
+}
