f903c327fa9e5d202a48793c21c414500bb44e7d
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-06-26 09:34:01 +0530

    Time: 318 ms (95.96%), Space: 0B (100.00%) - LeetHub

---
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM
+    person p
+    LEFT JOIN address a ON p.personId = a.personId;
