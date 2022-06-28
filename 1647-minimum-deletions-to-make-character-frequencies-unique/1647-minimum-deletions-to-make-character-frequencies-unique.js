a10b3877599221f832251aa3687e364031d43e3f
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-06-28 22:36:40 +0530

    Time: 217 ms (51.47%), Space: 48.1 MB (59.20%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minDeletions = function(s) {
+    const freq = {};
+    for (let c of s) {
+        freq[c] = (freq[c] || 0) + 1;
+    }
+    
+    let values = Object.values(freq).sort((a,b) => a - b);
+    let set = new Set();
+    let res = 0;
+    for (let i = 0; i < values.length; i++) {
+        while (set.has(values[i]) && values[i] !== 0) { // O(n^2)
+            values[i]--;
+            res++;
+        }
+        
+        set.add(values[i]);
+    }
+    
+    return res;
+};
