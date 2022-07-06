28f634851ad33eae86e9147f066a935362a2bc6e
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-07-06 11:33:30 +0530

    Time: 118 ms (28.87%), Space: 42.2 MB (15.73%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,14 +3,7 @@
  * @return {number}
  */
 var fib = function(n) {
-    
-    var helper = (n,arr) => {
-        if(n<=1) return n;
-        if(arr[n]!==-1) return arr[n];
-        return arr[n]=helper(n-1,arr)+helper(n-2,arr);
-    }
-    
-    var arr=new Array(n+1).fill(-1);
-    console.log(arr)
-    return helper(n,arr);
+    if(n===0) return 0;
+    if(n===1) return 1;
+    return fib(n-1)+fib(n-2);
 };
