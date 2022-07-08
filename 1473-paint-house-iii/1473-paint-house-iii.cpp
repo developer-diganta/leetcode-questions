2a566eb460cdb79f5d696a7a13f7e1a114875c7b
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-07-08 23:33:09 +0530

    Time: 221 ms (17.82%), Space: 17.6 MB (29.25%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<int> H;
+    vector<vector<int>> C;
+    int M, N, T, INF = 1e9 + 7;
+    vector<vector<vector<int>>> memo;
+    int dp(int i, int last, int cnt) {
+        if (cnt > T) return INF;
+        if (i == M) return cnt == T ? 0 : INF;
+        if (memo[i][last][cnt] != -1) return memo[i][last][cnt];
+        if (H[i]) return memo[i][last][cnt] = dp(i + 1, H[i], H[i] == last ? cnt : (cnt + 1));
+        int ans = INF;
+        for (int j = 0; j < N; ++j) ans = min(ans, C[i][j] + dp(i + 1, j + 1, j + 1 == last ? cnt : (cnt + 1)));
+        return memo[i][last][cnt] = ans;
+    }
+public:
+    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
+        H = houses, C = cost, M = m, N = n, T = target;
+        memo.assign(M, vector<vector<int>>(N + 1, vector<int>(T + 1, -1)));
+        int ans = dp(0, 0, 0);
+        return ans == INF ? -1 : ans;
+    }
+	// If you find helpful than upvote
+};
