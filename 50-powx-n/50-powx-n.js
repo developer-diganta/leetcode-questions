77ca44de05d08185b389064b8d3254ce353cc911
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-07-16 20:37:12 +0530

    Time: 75 ms (79.37%), Space: 42.3 MB (39.30%) - LeetHub

--- 50-powx-n/50-powx-n.js
+++ 50-powx-n/50-powx-n.js
@@ -3,19 +3,19 @@
  * @param {number} n
  * @return {number}
  */
-function helper(x,n){
-    if(n === 0) return 1;
-    if(x===0) return 0;
-    
-    var h = helper(x,Math.floor(n/2));
-    if(n%2 === 1){
-        return h*h*x;
-    }
-    else
-        return h*h;
-}
 var myPow = function(x, n) {
-    var  r = helper(x,Math.abs(n));
-    return r = n>=0?r:1/r;
+    var helper = (x,n) => {
+        if(x===0) return 0;
+        if(n===0) return 1;
+        var res = helper(x*x,Math.floor(n/2));
+        if(n%2===0){
+            return res;
+        }
+        else{
+            return x*res;
+        }
+    }
+    var t = helper(x,Math.abs(n));
+    if(n<0) return 1/t;
+    else return t;
 };
-
