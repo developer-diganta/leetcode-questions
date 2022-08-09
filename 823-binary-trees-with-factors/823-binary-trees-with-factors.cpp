ad4124de3b8f611a4512a9df7624093c0bc64006
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-08-09 22:11:51 +0530

    Time: 97 ms (66.15%), Space: 9.2 MB (67.38%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+            map<int,long long int>m;
+        sort(arr.begin(),arr.end());
+        
+        for(int x=0;x<arr.size();x++)
+        {
+            m.insert({arr[x],1});
+        }
+        
+        for(int i=1;i<arr.size();i++)
+        {  auto it3 = m.find(arr[i]);
+            long long int count =0;
+            for(int j=0;j<i;j++)
+            { 
+                if(arr[i]%arr[j]==0)
+                {   
+                    auto it =m.find(arr[i]/arr[j]); 
+                    auto it2 = m.find(arr[j]); 
+                    if(it!=m.end())
+                    {
+                        count+=(it->second)*(it2->second);
+                    }
+                }
+                
+            }
+            
+           it3->second+=count; 
+            
+        }
+        long long int sum =0;
+        for(auto it4=m.begin();it4!=m.end();it4++)
+        {
+            sum+= it4->second;
+        }
+       
+     return sum%(1000000007);
+    }
+};
