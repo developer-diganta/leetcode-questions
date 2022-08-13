29bd068f091d3cc5602d6cc756d91d4bb9051336
Diganta Kr Banik <65999534+developer-diganta@users.noreply.github.com>
2022-08-13 18:03:17 +0530

    Time: 97 ms (79.06%), Space: 52 MB (33.73%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(root===null || root===p || root===q){
+        return root;
+    }
+    var left = lowestCommonAncestor(root.left,p,q);    
+    var right = lowestCommonAncestor(root.right,p,q);
+    
+    if(left===null) return right;
+    if(right===null) return left;
+    return root;
+
+};
